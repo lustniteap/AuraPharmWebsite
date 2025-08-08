4cd873d14b6d790540898865ec8120c3321727a0
lustnite <4lustnite@gmail.com>
2025-08-07 20:24:48 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Array of slideshow images
+    const images = [
+      "6302C11D-6D1F-4E90-B52D-236815254D3D.jpg",
+      "72F0827B-5D2B-41DC-A26E-05A8A0B137B7.jpg",
+      "A93AD5BF-2CFB-4117-9956-6DB8D86ABBA1.jpg"
+    ];
+
+    let currentIndex = 0;
+    const hero = document.getElementById("hero");
+
+    // Function to change background
+    function changeBackground() {
+      hero.style.backgroundImage = `url('${images[currentIndex]}')`;
+      currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    // Initial background
+    changeBackground();
+
+    // Change image every 5 seconds
+    setInterval(changeBackground, 5000);
